f18c0d79fdfdd649bde360d56aa136847f1a1192
KotMilkMeoWtwo <forgithubkotmilkmeow@gmail.com>
2022-10-14 22:16:44 +0300

    nya

--- src/main/java/playercheck/ru/meowland/player/check/commands/CallOnCheckCmd.java
+++ src/main/java/playercheck/ru/meowland/player/check/commands/CallOnCheckCmd.java
@@ -1,12 +1,16 @@
 package playercheck.ru.meowland.player.check.commands;
 
 import com.destroystokyo.paper.event.player.PlayerSetSpawnEvent;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 import playercheck.ru.meowland.player.check.PlayerCheck;
+
+import java.util.Objects;
 
 public class CallOnCheckCmd implements CommandExecutor {
 
@@ -22,6 +26,10 @@ public class CallOnCheckCmd implements CommandExecutor {
 
         if(command.getName().equalsIgnoreCase("calloncheck")){
             Player p1 =  plugin.getServer().getPlayer(args[0]);
+            for(Player player : Bukkit.getOnlinePlayers()){
+                player.sendMessage(p1.getName() + ChatColor.translateAlternateColorCodes('&', Objects.requireNonNull(plugin.getConfig().getString("callforcheck"))));
+                player.sendMessage(p1.getName() + ChatColor.translateAlternateColorCodes('&', Objects.requireNonNull(plugin.getConfig().getString("callforcheck"))));
+            }
             return true;
         }
 
