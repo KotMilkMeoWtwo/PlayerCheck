348c00432508a9455468aaf4f4bf128cf6d94771
KotMilkMeoWtwo <forgithubkotmilkmeow@gmail.com>
2022-10-14 22:18:53 +0300

    nya

--- src/main/java/playercheck/ru/meowland/player/check/PlayerCheck.java
+++ src/main/java/playercheck/ru/meowland/player/check/PlayerCheck.java
@@ -1,12 +1,13 @@
 package playercheck.ru.meowland.player.check;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import playercheck.ru.meowland.player.check.commands.CallOnCheckCmd;
 
 public final class PlayerCheck extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        getCommand("calloncheck").setExecutor(new CallOnCheckCmd(this));
 
     }
 
