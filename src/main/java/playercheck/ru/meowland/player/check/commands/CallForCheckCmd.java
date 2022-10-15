4114f0aae0e8dd3ffbf0b74cabcc6fb998bec323
KotMilkMeoWtwo <forgithubkotmilkmeow@gmail.com>
2022-10-15 14:54:09 +0300

    nya

--- src/main/java/playercheck/ru/meowland/player/check/commands/CallForCheckCmd.java
+++ src/main/java/playercheck/ru/meowland/player/check/commands/CallForCheckCmd.java
@@ -6,15 +6,21 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerQuitEvent;
 import org.jetbrains.annotations.NotNull;
 import playercheck.ru.meowland.player.check.PlayerCheck;
 
 import java.util.Objects;
+import java.util.PriorityQueue;
 
-public class CallForCheckCmd implements CommandExecutor {
+public class CallForCheckCmd implements CommandExecutor, Listener {
 
     PlayerCheck plugin;
-
+    private Boolean meow;
+    private Player player;
     public CallForCheckCmd(PlayerCheck nya){
         this.plugin = nya;
     }
@@ -22,9 +28,10 @@ public class CallForCheckCmd implements CommandExecutor {
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
 
         Player p = (Player) sender;
-
         if(command.getName().equalsIgnoreCase("callforcheck")){
             Player p1 =  plugin.getServer().getPlayer(args[0]);
+            player = p1;
+            meow = true;
             for(Player player : Bukkit.getOnlinePlayers()){
                 player.sendMessage(p1.getName() + ChatColor.translateAlternateColorCodes('&', Objects.requireNonNull(plugin.getConfig().getString("callforcheck"))));
                 player.sendMessage(p1.getName() + ChatColor.translateAlternateColorCodes('&', Objects.requireNonNull(plugin.getConfig().getString("callforcheck"))));
@@ -34,4 +41,14 @@ public class CallForCheckCmd implements CommandExecutor {
 
         return false;
     }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void leaveEvent(PlayerQuitEvent e){
+        Player p = e.getPlayer();
+        if(p == player && meow){
+            p.banPlayerFull("Leave in check time");
+        }
+
+    }
+
 }
